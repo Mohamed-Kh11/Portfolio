edb771feb215d92001589bd2e00e271e484e4d15
Mohamed k <mkhaled11@outlook.com>
2025-11-13 16:29:50 +0200

    metadata back to page.js

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -7,6 +7,44 @@ import ScrollToTopButton from "./components/ScrollToTopButton";
 import Projects from "./components/Projects";
 
 
+export const metadata = {
+  title: {
+    default: "Mohamed's Portfolio",
+  },
+  description:
+    "Mohamed Khaled — Frontend Developer creating fast, responsive, and accessible web applications using React, Next.js, and Tailwind CSS. Passionate about design, performance, and user experiences.",
+  metadataBase: new URL("https://mohamed-kh.netlify.app"),
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Mohamed Khaled — Frontend Developer",
+    description:
+      "creating fast, responsive, and accessible web applications using React, Next.js, and Tailwind CSS. Passionate about design, performance, and user experiences.",
+    url: "https://mohamed-kh.netlify.app/",
+    siteName: "Mohamed Khaled",
+    images: [
+      {
+        url: "/link.png",
+        width: 1200,
+        height: 630,
+        alt: "Mohamed Khaled — Frontend Developer",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mohamed Khaled",
+    description:
+      "Frontend Developer, Passionate about design, performance, and user experiences. ",
+    images: ["/images/link.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function Home() {
   return (
